5a9b18908d59220679ea4545da54740aef49c6fa
reem <tarekreem950@gmail.com>
2024-03-18 02:53:38 +0200

    file and doc

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""lock boxes module"""
+
+
+def canUnlockAll(boxes):
+    """Lockboxes
+    Args:
+        boxes: boxes to check if it can be unlocked
+    Returns:
+        true or false
+    """
