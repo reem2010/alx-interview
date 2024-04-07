b531883cf4d5bb4c0cd0f0662d5189f32401a12f
reem <tarekreem950@gmail.com>
2024-04-07 20:16:58 +0200

    unicode

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,12 +9,22 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding
     """
-    for i in range(len(data)):
-        if data[i] >= 0 and data[i] <= 127:
-            data[i] = True
-        else:
-            data[i] = False
-    for res in data:
-        if not res:
+    i = 0
+    while i < len(data):
+        if 194 <= data[i] <= 223 and i < len(data) - 1:
+            if not (128 <= data[i + 1] <= 191):
+                return False
+            i += 2
+        elif 224 <= data[i] <= 239 and i < len(data) - 2:
+            if not(128 <= data[i + 1] <= 191 and 128 <= data[i + 2] <= 191):
+                return False
+            i += 3
+        elif 240 <= data[i] <= 247 and i < len(data) - 3:
+            if not(all(128 <= byte <= 191 for byte in data[i+1:i+4])):
+                return False
+            i += 4
+        elif not(data[i] <= 127):
             return False
+        else:
+            i += 1
     return True
