2d488139db8dedbe7eecdf5cc71782ef5cb39529
reem <tarekreem950@gmail.com>
2024-04-07 04:35:24 +0200

    preparing files for utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""UTF-8 Validation module"""
+
+
+def validUTF8(data):
+    """UTF-8 Validation
+    Args:
+        data: entred data
+    Returns:
+        True if data is a valid UTF-8 encoding
+    """
+    return True
