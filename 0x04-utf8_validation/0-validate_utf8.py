f127eb8b4168a001f39b8c9596148b32c8bb11f4
reem <tarekreem950@gmail.com>
2024-04-07 15:30:13 +0200

    unicode

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,4 +9,13 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding
     """
+    for i in range(len(data)):
+        if data[i] >= 0 and data[i] <= 127:
+            data[i] = True
+        elif(data[i] & 192 and i < len(data)-1):
+            data[i] = True
+            data[i + 1] = True
+            i = i + 1
+        else:
+            return False
     return True
