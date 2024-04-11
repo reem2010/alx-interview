f944a342f207febe9ee9b15c9a80020512b648e8
reem <tarekreem950@gmail.com>
2024-04-12 01:23:49 +0200

    preparing for n queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,16 +2,19 @@
 """N Queens module"""
 import sys
 
+
 def main():
     """n queens func"""
     if (len(sys.argv) != 2):
         print('Usage: nqueens N\n')
         sys.exit(1)
-    if (type(sys.argv[1]) != int):
+    if (not(sys.argv[1].isdigit())):
         print('N must be a number\n')
         sys.exit(1)
-    if (sys.argv[1] < 4):
+    if (int(sys.argv[1]) < 4):
         print('N must be at least 4\n')
         sys.exit(1)
+
+
 if __name__ == '__main__':
     main()
