a91a1cc8da7992cc07937bd7c3d31787785c544e
reem <tarekreem950@gmail.com>
2024-04-12 18:16:00 +0200

    removing newline

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -9,10 +9,10 @@ def main():
         print('Usage: nqueens N')
         sys.exit(1)
     if (not(sys.argv[1].isdigit())):
-        print('N must be a number\n')
+        print('N must be a number')
         sys.exit(1)
     if (int(sys.argv[1]) < 4):
-        print('N must be at least 4\n')
+        print('N must be at least 4')
         sys.exit(1)
 
 
