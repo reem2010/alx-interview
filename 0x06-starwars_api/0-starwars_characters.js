703f269040affe0d4b981786378613cb58fa016c
reem <tarekreem950@gmail.com>
2024-04-19 01:38:49 +0200

    request to api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,14 +3,14 @@ const request = require('request');
 if (process.argv.length === 3) {
   request('https://swapi-api.alx-tools.com/api/films', (error, response, body) => {
     if (error) {
-      console.log(error);
+        throw err;
     } else {
       body = JSON.parse(body);
       const peoples = body.results[[process.argv[2]]].characters;
       peoples.map((url) => {
         request(url, (error, response, body) => {
           if (error) {
-            console.log(error);
+            throw err;
           } else {
             body = JSON.parse(body);
             console.log(body.name);
