6bd925d15c4d37090fb12e20ce7dfe4e3968b11d
reem <tarekreem950@gmail.com>
2024-04-19 02:35:20 +0200

    request to api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,34 @@
 #!/usr/bin/node
 const request = require('request');
+const { promisify } = require('util');
+const promisifiedRequest = promisify(request);
 // if (process.argv.length === 3) {
-  request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, response, body) => {
-    if (error) {
-      throw error;
-    } else {
-      body = JSON.parse(body);
-      const peoples = body.characters;
-      peoples.map((url) => {
-        request(url, (error, response, body) => {
-          if (error) {
-            throw error;
-          } else {
-            body = JSON.parse(body);
-            console.log(body.name);
-          }
-        });
-        return url;
-      });
-    }
-  });
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, async (error, response, body) => {
+  if (error) {
+    // throw error;
+  } else {
+    body = JSON.parse(body);
+    let peoples = body.characters;
+    peoples = await Promise.all(peoples.map(async (url) => {
+      const body = await promisifiedRequest(url);
+      const data = JSON.parse(body.body);
+      //   console.log(body)
+      return data.name;
+    //   .then(({ body }) => {
+    //     const data = JSON.parse(body);
+    //     })
+    //   })(url, (error, response, body) => {
+    //     if (error) {
+    //       throw error;
+    //     } else {
+    //       body = JSON.parse(body);
+    //       name = body.name;
+    //       console.log(name)
+    //       console.log(ind)
+    //     }
+    //   });
+    }));
+    peoples.forEach((data) => console.log(data));
+  }
+});
 // }
