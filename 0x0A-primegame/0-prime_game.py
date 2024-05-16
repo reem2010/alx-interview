c37a0bfda53a19e36eccc42bfa6d0723bb1b4311
reem <tarekreem950@gmail.com>
2024-05-16 23:10:54 +0300

    package

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,11 +6,11 @@ def isWinner(x, nums):
     """Prime Game"""
     Ben = 0
     maria = 0
-    for i in range(x):
-        if (nums[i] == 1):
-            Ben = Ben + 1
-        else:
-            maria = maria + 1
+    # for i in range(x):
+        # if (nums[i] == 1):
+        #     Ben = Ben + 1
+        # else:
+        #     maria = maria + 1
         # arr = [0] * nums[i]
         # for j in range(1, nums[i]):
         #     if (j % 2):
