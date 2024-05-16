03ab9eff55e75ee9fe863772d27a31a798c0a23d
reem <tarekreem950@gmail.com>
2024-05-17 02:39:57 +0300

    prime algorithm

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,4 +6,26 @@ def isWinner(x, nums):
     """Prime Game"""
     Ben = 0
     maria = 0
-    return "Ben"
+    for i in nums:
+        arr = [x for x in range(2, i + 1)]
+        turn = 0
+        while (len(arr) > 0):
+            val = arr[0]
+            mul = 2
+            if (turn % 2):
+                Ben = Ben + 1
+                while val * mul <= i:
+                    arr.remove(val * mul)
+                    mul = mul + 1
+                arr.remove(val)
+            else:
+                maria = maria + 1
+                while val * mul <= i:
+                    arr.remove(val * mul)
+                    mul = mul + 1
+                arr.remove(val)
+            turn = turn + 1
+    if(Ben >= maria):
+        return "Ben"
+    else:
+        return "Maria"
