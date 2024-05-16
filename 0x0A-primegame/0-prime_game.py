bf5d656417e63028c3b3f57917b88b08b9311159
reem <tarekreem950@gmail.com>
2024-05-16 23:13:05 +0300

    maria

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,15 +6,4 @@ def isWinner(x, nums):
     """Prime Game"""
     Ben = 0
     maria = 0
-    # for i in range(x):
-        # if (nums[i] == 1):
-        #     Ben = Ben + 1
-        # else:
-        #     maria = maria + 1
-        # arr = [0] * nums[i]
-        # for j in range(1, nums[i]):
-        #     if (j % 2):
-        #         ben = ben + 1
-        #     else:
-        #         maria = maria + 1
-    return "Maria"
+    return "Ben"
