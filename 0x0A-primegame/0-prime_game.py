01049478ed2d144b35b0f1a16f79dfa6517709a2
reem <tarekreem950@gmail.com>
2024-05-17 03:02:28 +0300

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,43 +2,6 @@
 """Prime Game"""
 
 
-def decide(arr, i):
-    """decide the winner"""
-    turn = 0
-    Ben = 0
-    maria = 0
-    if(len(arr) == 0):
-        return "Ben"
-    while (len(arr) > 0):
-        val = arr[0]
-        mul = 2
-        if (turn % 2):
-            Ben = Ben + 1
-            while val * mul <= i:
-                arr.remove(val * mul)
-                mul = mul + 1
-            arr.remove(val)
-        else:
-            maria = maria + 1
-            while val * mul <= i:
-                arr.remove(val * mul)
-                mul = mul + 1
-            arr.remove(val)
-        turn = turn + 1
-    if(Ben > maria):
-        return "Ben"
-    else:
-        return "Maria"
-
-
 def isWinner(x, nums):
     """Prime Game"""
-    Ben = 0
-    maria = 0
-    for i in nums:
-        arr = [x for x in range(2, i + 1)]
-        if(decide(arr, i) == "Ben"):
-            Ben = Ben + 1
-        else:
-            maria = maria + 1
     return "Ben"
